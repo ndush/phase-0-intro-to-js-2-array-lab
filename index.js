d45b7ec8361b60c591ecd969285d910ebb2cd9f9
damaris <mdamarisnduku@gmail.com>
2022-06-10 12:57:28 +0300

    commit tree 441348730538b759dba69986ad311179ff39108e

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
 const cats = ["Milo", "Otis", "Garfield"];
 
-function destructivelyAppendCat (name) {
-cats.push(name);
+function destructivelyAppendCat(name) {
+  cats.push(name);
 }
 
-function destructivelyPrependCat (name) {
-cats.unshift(name);
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
 }
 
-function destructivelyRemoveLastCat(name){
-    cats.pop(name);
+function destructivelyRemoveLastCat(name) {
+  cats.pop(name);
 }
 
-function destructivelyRemoveFirstCat(name){
-    cats.shift(name);
-
+function destructivelyRemoveFirstCat(name) {
+  cats.shift(name);
 }
 
-function appendCat(name){
-    return[...cats,name];
-
+function appendCat(name) {
+  return [...cats, name];
 }
 
-function prependCat(name){
-    return[name,...cats];
+function prependCat(name) {
+  return [name, ...cats];
 }
-    
-function removeLastCat () {
-    return cats.slice(0, cats.length - 1);
-    }
-function removeFirstCat (){
-    return cats.slice(1);
+
+function removeLastCat() {
+  return cats.slice(0, cats.length -1);
+}
+function removeFirstCat() {
+  return cats.slice(1);
 }
